26716ad8296e95782583a24bea9b35ab80321a24
svirins <svirins@gmail.com>
2019-12-18 10:47:09 +0300

    server v0.2 groupList resolver ready

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,15 +1,36 @@
-function groupList(parent, args, context)  {
-  return context.prisma.groups()
+async function groupList(parent, args, context)  {
+  const where = args.filter
+    ? {
+        OR: [
+          { name_contains: args.filter },
+          { description_contains: args.filter },
+          { city_contains: args.filter },
+          { address_contains: args.filter },
+        ],
+      }
+    : {}
+  
+  const groups = await context.prisma.groups({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  })
+  const count = await context.prisma
+    .groupsConnection({
+      where,
+    })
+    .aggregate()
+    .count()
+
+  return {
+    groups,
+    count
+  }
+  
 }
 
 module.exports = {
   groupList,
 }
 
-/*
-group(id: ID!): Group
-groupByName(name: String): Group
-groups: [Group!]
-meeting(id: ID!): Meeting
-meetings: [Meeting!]
-*/
